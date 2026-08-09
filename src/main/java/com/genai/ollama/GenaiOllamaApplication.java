9abbf6f01ed44a9417cc3394621b2336256aacfd
manivannancsmca <manivannancsmca@gmail.com>
2026-08-09 22:38:19 +0530

    first commit

---
+++ src/main/java/com/genai/ollama/GenaiOllamaApplication.java
@@ -0,0 +1,13 @@
+package com.genai.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaiOllamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genai/ollama/GenaiOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genai.ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenaiOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
